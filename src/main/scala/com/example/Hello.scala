009998eec09dfffc7b0c80960901c3128a498b00
joe <joe@eyeem.com>
2016-04-29 11:47:20 +0200

    example Hello class doing one request to github

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,50 @@
 package com.example
 
-object Hello {
+import akka.stream.{ActorMaterializer, Materializer}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import play.api.libs.ws.{WSResponse, WSRequest}
+import play.api.libs.ws.ahc.AhcWSClient
+
+import scala.concurrent.Future
+import scala.util.Try
+
+object Hello extends LazyLogging {
+
+
+  implicit val actorSystem = akka.actor.ActorSystem()
+
+  implicit val materializer: Materializer = ActorMaterializer()(actorSystem)
+
+  implicit val wsClient = AhcWSClient()(materializer)
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def cleanUp() = {
+    logger.debug("cleaning up, closing wsClient and actorsystem")
+    wsClient.close()
+    actorSystem.terminate()
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    logger.debug("Hello, world!")
+
+    val organizationsUrl: String = "https://api.github.com/users/jschaul/orgs"
+
+    val request: WSRequest = wsClient.url(organizationsUrl)
+
+    val responseFuture: Future[WSResponse] = request.get()
+
+    responseFuture.map { response =>
+      logger.info(s"response: ${response.body}")
+      cleanUp()
+    }
+
+    responseFuture.onComplete { f: Try[WSResponse] =>
+      // this adds a second callback to the future created above
+      // and cleans up resources irrespective of whether the request completed successfully or with an exception
+      cleanUp()
+    }
+
+    logger.debug("finished Hello.scala")
   }
 }
